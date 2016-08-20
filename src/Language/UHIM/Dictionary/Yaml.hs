1038a604d929b78391f5d909f53fb946e44c0668
na4zagin3 <mrty.ityt.pt@gmail.com>
2016-08-20 12:50:55 -0500

    #14: Change kanji形 from [String] to Map String String

--- src/Language/UHIM/Dictionary/Yaml.hs
+++ src/Language/UHIM/Dictionary/Yaml.hs
@@ -63,7 +63,7 @@ deriveJSON defaultOptions ''KanjiShapes
 
 data KanjiDeclaration = KanjiDeclaration { kanji體 :: KanjiShapes
                                          , kanji音 :: [Pron]
-                                         , kanji形 :: [String]
+                                         , kanji形 :: Map String String
                                          , kanji義 :: Maybe [String]
                                          , kanji鍵 :: Maybe (Map String String)
                                          , kanji頻度 :: Maybe Double
