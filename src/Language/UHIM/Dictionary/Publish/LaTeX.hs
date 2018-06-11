5700e0457ba75b8c61e17a0fa2bf870018d39745
na4zagin3 <mrty.ityt.pt@gmail.com>
2018-06-11 23:47:59 +0900

    #10 Show frequency, key, and shape in LaTeX

--- src/Language/UHIM/Dictionary/Publish/LaTeX.hs
+++ src/Language/UHIM/Dictionary/Publish/LaTeX.hs
@@ -88,12 +88,32 @@ meaning _ ss = mconcat [ "\\begin{MeaningPl}\n"
                        , "\\end{MeaningPl}\n"
                        ]
 
+emitFrequency :: (IsString s, Monoid s) => Config -> Maybe Double -> s
+emitFrequency _ Nothing = ""
+emitFrequency _ (Just f) = mconcat ["\\Frequency{", fromString $ show f, "}\n"]
+
 tags :: (IsString s, Monoid s) => Config -> [String] -> s
 tags _ [] = ""
 tags _ ss = mconcat [ "\\begin{Tags}\n"
                     , mconcat $ map (\s -> mconcat ["\\Tag{", escape s, "}\n"]) ss
                     , "\\end{Tags}\n"
                     ]
+
+emitKeys :: (IsString s, Monoid s) => Config -> (Map String String) -> s
+emitKeys _ ks | M.null ks = ""
+emitKeys _ ks = mconcat
+  [ "\\begin{Keys}\n"
+  , mconcat $ map (\(t, s) -> mconcat ["\\Key{", escape t, "}{", escapeWithVisibleSpace True s, "}%\n"]) $ M.toList ks
+  , "\\end{Keys}\n"
+  ]
+
+emitShapes :: (IsString s, Monoid s) => Config -> Map String String -> s
+emitShapes _ ks | M.null ks = ""
+emitShapes _ ks = mconcat
+  [ "\\begin{Shapes}\n"
+  , mconcat $ map (\(t, s) -> mconcat ["\\Shape{", escape t, "}{", escape s, "}%\n"]) $ M.toList ks
+  , "\\end{Shapes}\n"
+  ]
 
 
 headWord :: (IsString s, Eq s) => String -> [WordConvPair] -> [s]
@@ -130,60 +150,74 @@ emitPosition :: (IsString s, Monoid s, Eq s) => Position -> s
 emitPosition = mconcat . mconcat . map (\(f,p) -> [ "\\Position{", escape f, "}{", escape $ show p, "}"])
 
 emitEntry :: (IsString s, Monoid s, Eq s) => Config -> (Position, DictEntry) -> s
-emitEntry c (pos, Entry字 decl) = mconcat
+emitEntry c (pos, d@(Entry字 decl)) = mconcat
   [ emitPosition pos
-  , "\n"
+  , "%\n"
   , emitHeadKanjiShapes $ kanji體 decl
-  , "\n"
+  , "%\n"
+  , emitFrequency c $ frequency d
+  , "%\n"
+  , fromMaybe "" (emitShapes c <$> kanji形 decl)
+  , "%\n"
+  , fromMaybe "" (emitKeys c <$> kanji鍵 decl)
+  , "%\n"
   , kanjiYomi c $ kanji音 decl
-  , "\n"
+  , "%\n"
   , fromMaybe "" (meaning c <$> kanji義 decl)
-  , "\n"
-  , fromMaybe "" (tags c <$> kanji簽 decl)
+  , "%\n"
+  , fromMaybe "" (tags c <$> entryLabel d)
   ]
 
-emitEntry c (pos, Entry語 decl) = mconcat
+emitEntry c (pos, d@(Entry語 decl)) = mconcat
   [ emitPosition pos
   , "\n"
   , mconcat $ headWord "語" $ word聯 decl
   , "\n"
+  , emitFrequency c $ frequency d
+  , "\n"
   , fromMaybe "" (meaning c <$> word義 decl)
   , "\n"
-  , fromMaybe "" (tags c <$> word簽 decl)
+  , fromMaybe "" (tags c <$> entryLabel d)
   ]
 
-emitEntry c (pos, Entry日副詞 decl) = mconcat
+emitEntry c (pos, d@(Entry日副詞 decl)) = mconcat
   [ emitPosition pos
   , "\n"
   , mconcat $ headWord "日副詞" $ word聯 decl
   , "\n"
+  , emitFrequency c $ frequency d
+  , "\n"
   , fromMaybe "" (meaning c <$> word義 decl)
   , "\n"
-  , fromMaybe "" (tags c <$> word簽 decl)
+  , fromMaybe "" (tags c <$> entryLabel d)
   ]
 
-emitEntry c (pos, Entry日動詞 decl) = mconcat
+emitEntry c (pos, d@(Entry日動詞 decl)) = mconcat
   [ emitPosition pos
   , "\n"
   , mconcat $ headWord "日動詞" $ jaVerb聯 decl
   , "\n"
   , verbConj $ jaVerb類 decl
   , "\n"
+  , emitFrequency c $ frequency d
+  , "\n"
   , fromMaybe "" (meaning c <$> jaVerb義 decl)
   , "\n"
-  , fromMaybe "" (tags c <$> jaVerb簽 decl)
+  , fromMaybe "" (tags c <$> entryLabel d)
   ]
 
-emitEntry c (pos, Entry日形容詞 decl) = mconcat
+emitEntry c (pos, d@(Entry日形容詞 decl)) = mconcat
   [ emitPosition pos
   , "\n"
   , mconcat $ headWord "日形容詞" $ jaAdj聯 decl
   , "\n"
   , adjConj $ jaAdj類 decl
   , "\n"
+  , emitFrequency c $ frequency d
+  , "\n"
   , fromMaybe "" (meaning c <$> jaAdj義 decl)
   , "\n"
-  , fromMaybe "" (tags c <$> jaAdj簽 decl)
+  , fromMaybe "" (tags c <$> entryLabel d)
   ]
 
 emitFilePaths :: (IsString s, Monoid s, Eq s) => [FilePath] -> s
@@ -222,8 +256,11 @@ emitDict c ds  = [ fromString $ template c
                          ,  "\\end{Entry}\n"
                          ]
 
-escapeChar :: (Stream s m Char) => ParsecT s u m String
-escapeChar = (oneOf "$%#{}&^" >>= \c -> return ("\\" ++ [c])) <|> (char '\\' >> return "\\textbackslash ")
+escapeChar :: (Stream s m Char) => Bool -> ParsecT s u m String
+escapeChar escapeSpace = (oneOf "$%#{}&^" >>= \c -> return ("\\" ++ [c])) <|> (char '\\' >> return "\\textbackslash ") <|> escSpace
+  where
+    escSpace | escapeSpace = char ' ' >> return "\\textvisiblespace "
+             | not escapeSpace = parserZero
 
 escapeHandakuten :: (Stream s m Char) => ParsecT s u m String
 escapeHandakuten = try $ do
@@ -231,11 +268,15 @@ escapeHandakuten = try $ do
   _ <- string "\x309a"
   return $ "{\\bou{" ++ [c] ++ "}}"
 
-escapeString :: (Stream s m Char) => ParsecT s u m String
-escapeString = fmap concat . many $ choice [ escapeHandakuten
-                                           , escapeChar
-                                           , anyChar >>= \c -> return [c]
-                                           ]
+escapeString :: (Stream s m Char) => Bool ->ParsecT s u m String
+escapeString escapeSpace = fmap concat . many $ choice
+  [ escapeHandakuten
+  , escapeChar escapeSpace
+  , anyChar >>= \c -> return [c]
+  ]
+
+escapeWithVisibleSpace :: (IsString s) => Bool -> String -> s
+escapeWithVisibleSpace escapeSpace s = fromString . either (error . show) id $ P.parse (escapeString escapeSpace) "LaTeX:escape" s
 
 escape :: (IsString s) => String -> s
-escape s = fromString . either (error . show) id $ P.parse escapeString "LaTeX:escape" s
+escape = escapeWithVisibleSpace False
